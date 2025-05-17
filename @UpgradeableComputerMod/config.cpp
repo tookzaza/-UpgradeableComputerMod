cf0939bcf4854dbe1d43cc857272e95bd2bee08c
tookzaza <terstoneey@gmail.com>
2025-05-16 19:24:02 -0600

    Update config.cpp
    d

--- @UpgradeableComputerMod/config.cpp
+++ @UpgradeableComputerMod/config.cpp
@@ -2,7 +2,13 @@ class CfgPatches
 {
     class UpgradeableComputerMod
     {
-        units[] = {};
+        units[] = {
+            "Upgradeable_ComputerBase",
+            "CircuitBoard_T1",
+            "CircuitBoard_T2",
+            "CircuitBoard_T3",
+            "CircuitBoard_T4"
+        };
         weapons[] = {};
         requiredVersion = 0.1;
         requiredAddons[] = {"DZ_Data"};
@@ -11,52 +17,50 @@ class CfgPatches
 
 class CfgVehicles
 {
-    class Inventory_Base;
+    class Container_Base;
 
-    class Upgradeable_ComputerBase: Inventory_Base
+    class Upgradeable_ComputerBase: Container_Base
     {
         scope = 2;
-        scopeInventory = 2;
-        simulation = "inventoryItem";
-
         displayName = "Upgradeable Computer";
-        descriptionShort = "A placeable computer that can be upgraded with circuit boards to increase storage.";
+        descriptionShort = "A placeable computer that can be upgraded with circuit boards.";
         model = "\UpgradeableComputerMod\models\computer.p3d";
-
-        itemSize[] = {4,4};
-        itemsCargoSize[] = {20, 5};
-
+        weight = 10000;
+        itemSize[] = {5,5};
+        itemsCargoSize[] = {10,10};
+        cargoIsOpen = 1;
+        varQuantityDestroyOnMin = 1;
+        physLayer = "item_large";
+        hiddenSelections[] = {};
+        hiddenSelectionsTextures[] = {};
+        attachments[] = {};
+        placement = "vertical";
+        canBeDigged = 0;
         canBePlaced = 1;
-        rotationFlags = 17;
-
+        lootCategory = "Crafted";
+        isMeleeWeapon = 0;
         allowOwnedCargoManipulation = 1;
-
-        // Optional: You can add attachments here if you want to allow upgrades as attachments
-        // attachments[] = {};
+        overrideDrawArea = "5.0";
+        class AnimationSources {};
+        class GUIInventoryAttachmentsProps {};
     };
+
+    class Inventory_Base;
 
     class CircuitBoard_Base: Inventory_Base
     {
         scope = 0;
-        scopeInventory = 0;
-        simulation = "inventoryItem";
-
         displayName = "Circuit Board";
         descriptionShort = "An upgrade circuit board.";
         model = "\UpgradeableComputerMod\models\circuitboard.p3d";
-
         itemSize[] = {1,1};
         weight = 50;
-
         canBeDropped = 1;
-        allowOwnedCargoManipulation = 1;
     };
 
     class CircuitBoard_T1: CircuitBoard_Base
     {
         scope = 2;
-        scopeInventory = 2;
-
         displayName = "Circuit Board Tier 1";
         descriptionShort = "Adds 250 slots when used on the computer.";
     };
@@ -64,8 +68,6 @@ class CfgVehicles
     class CircuitBoard_T2: CircuitBoard_Base
     {
         scope = 2;
-        scopeInventory = 2;
-
         displayName = "Circuit Board Tier 2";
         descriptionShort = "Adds 500 slots when used on the computer.";
     };
@@ -73,8 +75,6 @@ class CfgVehicles
     class CircuitBoard_T3: CircuitBoard_Base
     {
         scope = 2;
-        scopeInventory = 2;
-
         displayName = "Circuit Board Tier 3";
         descriptionShort = "Adds 750 slots when used on the computer.";
     };
@@ -82,8 +82,6 @@ class CfgVehicles
     class CircuitBoard_T4: CircuitBoard_Base
     {
         scope = 2;
-        scopeInventory = 2;
-
         displayName = "Circuit Board Tier 4";
         descriptionShort = "Adds 1000 slots when used on the computer.";
     };
